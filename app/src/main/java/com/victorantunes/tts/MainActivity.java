1cddf048c29d1684a6e0fb253a3fd206ca68daa3
victorantunes <victor.antunes.ignacio@gmail.com>
2016-01-07 20:44:02 -0300

    Added soft keyboard layout resize

---
+++ app/src/main/java/com/victorantunes/tts/MainActivity.java
@@ -0,0 +1,59 @@
+package com.victorantunes.tts;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText textField;
+    private TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textField = (EditText) findViewById(R.id.textField);
+    }
+
+
+    public void convertTextToSpeech(View v) {
+
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                tts.speak(textField.getText().toString(), TextToSpeech.QUEUE_ADD, null);
+            }
+        });
+
+    }
+
+
+    public void clearText(View v) {
+        textField.setText("");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
